fba2cc2bff4522750c9b6afc2d0c9149dbcf45a9
Bartosz Mody <terar8@gmail.com>
2025-07-29 19:35:13 +0200

    feat: implemented Middleware for handling exceptions

---
+++ ExchangeRateApp.WebApi/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using ExchangeRateApp.Common.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context); 
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unhandled exception occurred while processing request: {Method} {Path}",
+                context.Request.Method,
+                context.Request.Path);
+
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        int statusCode = exception switch
+        {
+            BadRequestException => StatusCodes.Status400BadRequest,
+            NotFoundException => StatusCodes.Status404NotFound,
+            ServiceUnavailableException => StatusCodes.Status503ServiceUnavailable,
+            GeneralException => StatusCodes.Status500InternalServerError,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        var result = JsonSerializer.Serialize(new
+        {
+            error = exception.Message,
+        });
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = statusCode;
+
+        return context.Response.WriteAsync(result);
+    }
+}
